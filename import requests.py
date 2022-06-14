bc6de488c9a61d23cb58976f8fb7debb47e73c23
mrbrat <shinev999@gmail.com>
2022-06-13 22:12:48 -0400

    shark

---
+++ import requests.py
@@ -0,0 +1,8 @@
+import requests
+city = 'ottawa'
+api_url = 'https://api.api-ninjas.com/v1/airquality?city={}'.format(city)
+response = requests.get(api_url, headers={'X-Api-Key': 'Mjvc+QDmZJZ0KEJCYu2L/Q==qxTZL4b8DNJ9zrTI'})
+if response.status_code == requests.codes.ok:
+    print(response.text)
+else:
+    print("Error:", response.status_code, response.text)
