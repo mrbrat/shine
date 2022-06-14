8b7f3ab8032fa15469a0717ac63cbc862f6831b9
mrbrat <shinev999@gmail.com>
2022-06-13 22:16:17 -0400

    city changed

--- import requests.py
+++ import requests.py
@@ -1,5 +1,5 @@
 import requests
-city = 'ottawa'
+city = 'toronto'
 api_url = 'https://api.api-ninjas.com/v1/airquality?city={}'.format(city)
 response = requests.get(api_url, headers={'X-Api-Key': 'Mjvc+QDmZJZ0KEJCYu2L/Q==qxTZL4b8DNJ9zrTI'})
 if response.status_code == requests.codes.ok:
